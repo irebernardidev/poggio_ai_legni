ce1a35dc66ed89679acb68df29f5a39b70087f41
Ire <123773847+irebernardidev@users.noreply.github.com>
2024-03-02 10:57:38 +0000

    trying fix navbar scroll

--- js/script.js
+++ js/script.js
@@ -1,39 +1,33 @@
 const navbar = document.getElementById('navbar');
-   let scrolled = false;
+let scrolled = false;
 
-   window.onscroll = function () {
-      if (window.pageYOffset > 100) { // Adjusted to 100 for demonstration
-         navbar.classList.remove('top');
-         navbar.classList.add('scrolled'); // Add class when scrolled
-         if (!scrolled) {
-            navbar.style.transform = "translateY(-70px)";
-         }
-         setTimeout(function () {
-            navbar.style.transform = "translateY(0)";
-            scrolled = true;
-         }, 250);
-      } else {
-         navbar.classList.add('top');
-         navbar.classList.remove('scrolled'); // Remove class when not scrolled
-         scrolled = false;
-      }
-   }
+window.onscroll = function() {
+  if (window.scrollY > 500) {
+    navbar.classList.remove('top')
+    if (!scrolled) {
+      navbar.style.transform = "translateY(-70px)";
+    }
+    setTimeout(function() {
+      navbar.style.transform = "translateY(0)"; 
+      scrolled = true;
+    }, 250);
+  } else {
+    navbar.classList.add('top');
+    scrolled = false; 
+  }
+}
 
-   // Smooth Scrolling
-   $('#navbar a').on('click', function (e) {
-      if (this.hash !== '') {
-         e.preventDefault();
+// Smooth Scrolling
+$('#navbar a').on('click', function (e) {
+  if (this.hash !== '') { 
+    e.preventDefault();
+    const hash = this.hash;
 
-         const hash = this.hash;
-
-         $('html, body').animate(
-            {
-               scrollTop: $(hash).offset().top - 50,
-            },
-            900
-         );
-      }
-   });
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top - 50
+    }, 900);
+  }  
+});
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
